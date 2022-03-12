a8a24f6fad8eff535bfe998e9014add1cc034f67
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-03-12 15:36:51 +0100

    after 45. Debugging with Visual Studio Code

---
+++ s03-v34/src/app.ts
@@ -0,0 +1,21 @@
+// let age: number;
+// age = 30;
+// const userName = 'Maximilian';
+// console.log(userName);
+let appId = 'abc';
+
+const button = document.querySelector('button')!;
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+function clickHandler(message: string) {
+  // let userName = 'Max';
+  console.log('Clicked! ' + message);
+}
+
+button.addEventListener('click', clickHandler.bind(null, 'You\'re welcome!'));
