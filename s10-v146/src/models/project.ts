f13c3a1d10580f4d68785a3393a1a678b9f5ccfd
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-05-29 15:11:17 +0200

    after 145. Important: Use Chrome or Firefox

---
+++ s10-v146/src/models/project.ts
@@ -0,0 +1,16 @@
+namespace App {
+  export enum ProjectStatus {
+    Active, 
+    Finished
+  }
+  
+  export class Project {
+    constructor( // prop creation shorthand!
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectStatus
+    ) {}
+  }
+}
