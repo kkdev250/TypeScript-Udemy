d35ab441fe4734c2ad3f6e7dfa6b032b5bb01e4c
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-06-04 12:36:35 +0200

    after 157. Finishing the Setup & Adding webpack-dev-server

---
+++ s11-v154/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, './'),
+      }
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
