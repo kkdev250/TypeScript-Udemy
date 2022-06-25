4ecc9d77f063916d2532186e07526b18ee6de104
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-06-25 15:20:14 +0200

    after 173. Rendering a Map with Google Maps (incl. Types!)

--- s13-v169/src/app.ts
+++ s13-v169/src/app.ts
@@ -1,1 +1,43 @@
-// Code goes here!
+import axios from 'axios';
+
+const from = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = '';
+
+// declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: {geometry: {location: {lat: number, lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios.get<GoogleGeocodingResponse>(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+      enteredAddress
+    )}&key=${GOOGLE_API_KEY}`
+  ).then(response => {
+    console.log(response);
+    // if (response.data.status !== 'OK') {
+    //   throw new Error('Could not fetch location!');
+    // }
+    // const coordinates = response.data.results[0].geometry.location;
+    const coordinates = {lat: -34.397, lng: 150.644};
+    const map = new google.maps.Map(document.getElementById('map')!, {
+      center: coordinates,
+      zoom: 8
+    });
+
+    new google.maps.Marker({position: coordinates, map: map})
+  })
+  .catch(err => {
+    alert(err.message);
+    console.log(err);
+  });
+};
+
+from.addEventListener('submit', searchAddressHandler);
